f61bfbb56ef51203f48ce8d91dadf17c5e14111a
hanzo <enzodurandcontact@gmail.com>
2021-12-12 17:48:40 +0100

    update project

--- src/projet.pl
+++ src/projet.pl
@@ -1,4 +1,4 @@
-%% Donnees du projet, Abox et Tbox.
+%% =================== Donnees du projet, Abox et Tbox =================== %%
 
 equiv(sculpteur,and(personne,some(aCree,sculpture))). 
 equiv(auteur,and(personne,some(aEcrit,livre))). 
@@ -37,79 +37,100 @@ instR(michelAnge, david, aCree).
 instR(michelAnge, sonnets, aEcrit).
 instR(vinci, joconde, aCree).
 
-%% Partie principale
+%% =================== Partie principale =================== %%
 
-programme :-
+%% Lance les 3 etapes du projet avec les bons arguments
+programme :- 
   premiere_etape(Tbox,Abi,Abr),
   deuxieme_etape(Abi,Abe,Tbox),
   troisieme_etape(Abe,Abr).
 
+ %% La premiere etape consiste a creer la TBox, l'Abi et l'Abr
 premiere_etape(Tbox,Abi,Abr) :-
   creer_tbox(Tbox),
   creer_abi(Abi),
   creer_abr(Abr).
 
+%% La deuxieme etape est l'entree du programme, on choisit la
+%% proposition a demontrer avant de lancer la resolution
 deuxieme_etape(Abi,Abi1,Tbox) :-
   saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
 
+%% La troisieme etape fait un tri dans la Abox et lance la resolution
 troisieme_etape(Abi,Abr) :-
   tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
   resolution(Lie,Lpt,Li,Lu,Ls,Abr),
   nl, write('Resolution effectue').
 
-%% Fonction utile pour la premiere etape
+%% =================== Fonctions utiles pour la premiere etape =================== %%
 
 creer_tbox(Tbox) :- 
   setof((C,D), 
   equiv(C,D), Tbox).
+
 creer_abi(Abi) :- 
   setof((I,C), 
   inst(I,C), Abi).
+
 creer_abr(Abr) :- 
   setof((I1,I2,R), 
   instR(I1,I2,R), Abr).
 
-%% Fonction utile pour la deuxieme etape
+%% =================== Fonctions utiles pour la deuxieme etape =================== %% 
 
+%% Fonction faisant l'affichage demandant les entree et recuperant le type
+%% de proposition a demontrer
 saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox) :-
   nl, write('Entrez le numero du type de proposition que vous demontrez :'),
-  nl, write('1 Une instance donnee appartient a un concept donne.'),
-  nl, write('2 Deux concepts n"ont pas d"elements en commun(ils ont une intersection vide).'),
+  nl, write('1. Une instance donnee appartient a un concept donne.'),
+  nl, write('2. Deux concepts n\'ont pas d\'elements en commun (ils ont donc une intersection vide).'),
   nl, read(R), 
   suite(R,Abi,Abi1,Tbox).
 
+%% Fonction permettant de recuperer la proposition suivant le type de
+%% proposition choisit et de checker si l'utilisateur a bien entree
+%% 1 ou 2
 suite(1,Abi,Abi1,Tbox) :-
   acquisition_prop_type1(Abi,Abi1,Tbox),!.
 suite(2,Abi,Abi1,Tbox) :-
   acquisition_prop_type2(Abi,Abi1,Tbox),!.
 suite(_,Abi,Abi1,Tbox) :-
-  nl, write('Cette reponse est incorrecte.'),
+  nl, write('Reponse incorrecte.'),
   nl, saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
 
+%% Fonction permettant de tester si l'entree utilisateur pour les
+%% proposition de type 1 est correct. Si l'entree est correct alors
+%% on prend la negation et on la met sous forme normale negative
 acquisition_prop_type1(Abi,Abi1,Tbox) :-
-  nl, write('Entrez une instance :'),
+  nl, write('Entrer l\'instance :'),
   nl, read(I), testinstance(I),
-  nl, write('Entrez un concept :'),
+  nl, write('Entrer le concept :'),
   nl, read(C), testconcept(C),
   remplace(C,CC),
   nnf(not(CC),NCC),
   Abi1 = [(I,NCC)|Abi].
 
+%% Fonction permettant de tester si les entrees de l'utilisateur pour 
+%% les propositions de type 2 sont correct. Si les deux entrees sont 
+%% correct alors on met l'entree sous forme normale negative 
 acquisition_prop_type2(Abi,Abi1,Tbox) :-
-  nl, write('Entrez un concept (1) :'),
+  nl, write('Entrer le premier concept :'),
   nl, read(C1), 
   testconcept(C1),
-  nl, write('Entrez un concept (2) :'),
+  nl, write('Entrer le deuxieme concept :'),
   nl, read(C2), 
   testconcept(C2),
   remplace(and(C1,C2),CC),
   nnf(CC,NCC),
   Abi1 = [(I,NCC)|Abi].
 
-%% Fonction utile pour la troisieme etape
+%% =================== Fonctions utiles pour la troisieme etape =================== %% 
 
+%% Initialisation du compteur permettant de nommer nos nouvelles variables
 compteur(1).
 
+%% La fonction genere permet de generer de nouvelles variables lors de l'utilisation
+%% de la regle il existe
 genere(Nom) :-
   compteur(V), 
   nombre(V,L1), 
@@ -142,6 +163,9 @@ chiffre_car(7,'7').
 chiffre_car(8,'8'). 
 chiffre_car(9,'9').
 
+%% La fonction autoref permet de checker si il y a une reference circulaire dans
+%% notre Abox/Tbox. Cela pourrait faire tourner notre programme en boucle il est
+%% donc important de verifier cela
 autoref(C,C) :- 
   cnamea(C).
 autoref(C,C) :- 
@@ -197,6 +221,9 @@ autoref(C,some(_,D)) :-
   cnamea(C), 
   autoref(C,D).
 
+%% Cette fonction permet de tester si les variables utilisees dans nos regles
+%% sont bien a leur place. Par exemple il ne peut y avoir que deux concepts
+%% autour d'un and ou d'un or
 testconcept(C) :- 
   cnamea(C), !.
 testconcept(C) :- 
@@ -216,6 +243,7 @@ testconcept(all(R,C)) :-
   rname(R), 
   testconcept(C), !.
 
+%% Cette fonction remplace les entree par leurs definitions
 remplace(C,C) :- 
   cnamea(C).
 remplace(not(C),not(CC)) :- 
@@ -233,13 +261,16 @@ remplace(all(R,C),all(R,CC)) :-
 remplace(C,CC) :- equiv(C,D), 
   remplace(D,CC).
 
+%% Cette fonction permet de concatener deux listes
 concat([],L1,L1).
 concat([X|Y],L1,[X|L2]) :- 
   concat(Y,L1,L2).
 
+%% Cette fonction test si l'entree est bien une instance
 testinstance(I) :- 
   iname(I), !.
 
+%% Cette fonction met l'entree sous forme normale negative
 nnf(not(and(C1,C2)),or(NC1,NC2)):- 
   nnf(not(C1),NC1), 
   nnf(not(C2),NC2),!.
@@ -264,6 +295,7 @@ nnf(all(R,C),all(R,NC)) :-
   nnf(C,NC),!.
 nnf(X,X).
 
+%% Cette fonction trie la Abox avant la resolution
 tri_Abox([],[],[],[],[],[]).
 tri_Abox([(I,some(R,C))|Abi],[(I,some(R,C))|Lie],Lpt,Li,Lu,Ls) :- 
   tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
@@ -276,25 +308,33 @@ tri_Abox([(I,or(C1,C2))|Abi],Lie,Lpt,Li,[(I,or(C1,C2))|Lu],Ls) :-
 tri_Abox([E|Abi],Lie,Lpt,Li,Lu,[E|Ls]) :- 
   tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
 
+%% Cette fonction est la boucle principale de la resolution.
+%% A chaque appel, elle check premierement si il y a un clash.
+%% Elle check ensuite si on peut appliquer la regle il existe
 resolution(Lie,Lpt,Li,Lu,Ls,Abr):-
   checkclash(Ls).
 resolution(Lie,Lpt,Li,Lu,Ls,Abr):-
   complete_some(Lie,Lpt,Li,Lu,Ls,Abr).
 
+%% Cette fonction permet de check si il y a un clash dans notre
+%% liste Ls
 checkclash([(I,C)|Ls]) :-
   nnf(not(C),NC),
   member((I,NC),Ls).
 checkclash([_|Ls]) :- 
   checkclash(Ls).
 
+%% Cette fonction est appele a chaque modification de liste pendant
+%% la resolution
 evolue((I,and(A,B)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, [(I,and(A,B))|Li], Lu, Ls).
 evolue((I,all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, [(I,all(R,C))|Lpt], Li, Lu, Ls).
 evolue((I,some(R,C)), Lie, Lpt, Li, Lu, Ls, [(I,some(R,C))|Lie], Lpt, Li, Lu, Ls).
 evolue((I,or(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, [(I,or(C1,C2))|Lu], Ls).
 evolue(Elem, Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [(Elem)|Ls]).
 
-%% Predicats des differentes regles de resolution
+%% =================== Predicats des differentes regles de resolution =================== %% 
 
+%% Regle il existe
 complete_some([],Lpt,Li,Lu,Ls,Abr):-
   transformation_and([],Lpt,Li,Lu,Ls,Abr).
 complete_some([(A,some(R,C))|Lie],Lpt,Li,Lu,Ls,Abr) :-
@@ -303,6 +343,7 @@ complete_some([(A,some(R,C))|Lie],Lpt,Li,Lu,Ls,Abr) :-
   affiche_evolution_Abox(Ls,Lie,Lpt,Li,Lu,[],Ls1,Lie1,Lpt1,Li1,Lu1,[]),
   resolution(Lie1,Lpt1,Li1,Lu1,Ls1,[(A,B,R)|Abr]).
 
+%% Regle et
 transformation_and(Lie,Lpt,[],Lu,Ls,Abr):-
   deduction_all(Lie,Lpt,[],Lu,Ls,Abr).
 transformation_and(Lie,Lpt,[(I,and(A,B))|Li],Lu,Ls,Abr):-
@@ -312,6 +353,7 @@ transformation_and(Lie,Lpt,[(I,and(A,B))|Li],Lu,Ls,Abr):-
   affiche_evolution_Abox(Ls1,Lie1,Lpt1,Li1,Lu1,[],Ls2,Lie2,Lpt2,Li2,Lu2,[]),
   resolution(Lie2,Lpt2,Li2,Lu2,Ls2,Abr).
 
+%% Regle quelquesoit
 deduction_all(Lie,[],Li,Lu,Ls,Abr):-
   transformation_or(Lie,[],Li,Lu,Ls,Abr).
 deduction_all(Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls,Abr):-
@@ -320,6 +362,7 @@ deduction_all(Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls,Abr):-
   affiche_evolution_Abox(Ls,Lie,Lpt,Li,Lu,[],Ls1,Lie1,Lpt1,Li1,Lu1,[]),
   resolution(Lie1,Lpt1,Li1,Lu1,Ls1,Abr).
 
+%% Regle ou
 transformation_or(Lie,Lpt,Li,[(I,or(C,D))|Lu],Ls,Abr):- 
   evolue((I,C),Lie,Lpt,Li,Lu,Ls,Lie1,Lpt1,Li1,Lu1,Ls1),
   affiche_evolution_Abox(Ls,Lie,Lpt,Li,Lu,[],Ls1,Lie1,Lpt1,Li1,Lu1,[]),
@@ -329,8 +372,9 @@ transformation_or(Lie,Lpt,Li,[(I,or(C,D))|Lu],Ls,Abr):-
   resolution(Lie1,Lpt1,Li1,Lu1,Ls1,Abr),
   resolution(Lie2,Lpt2,Li2,Lu2,Ls2,Abr).
 
-%% Partie affichage
+%% =================== Partie affichage =================== %%
 
+%% Affiche l'evolution des qu'il y a eu une modification dans une des listes
 affiche_evolution_Abox(Ls1,Lie1,Lpt1,Li1,Lu1,Abr1,Ls2,Lie2,Lpt2,Li2,Lu2,Abr2):-
   print_diff(Ls2,Ls1),
   print_diff(Lie2,Lie1),
@@ -338,23 +382,36 @@ affiche_evolution_Abox(Ls1,Lie1,Lpt1,Li1,Lu1,Abr1,Ls2,Lie2,Lpt2,Li2,Lu2,Abr2):-
   print_diff(Li2,Li1),
   print_diff(Lu2,Lu1).
 
+%% Trouve une difference dans les listes et appelle la traduction prefixe - infixe
 print_diff(L1,L2):-
   diff_list(L1,L2,[E|R]),
   nl,
   trad_infix(E).
 
+%% Retourne les elements differents entre deux listes
 diff_list(L1,L2,R) :- 
   findall(E,(member(E,L1),not(member(E,L2))),R).
 
+%% Affiche la traduction infixe d'une entree prefixe
 trad_infix([]).
 trad_infix((I,not(A))):-
   write(I), 
   write(":no("),
   trad_infix(A),
   write(")").
+trad_infix(not(A)):-
+  write("no("),
+  trad_infix(A),
+  write(")").
 trad_infix((I,and(A,B))):-
   write(I), 
   write(":("),
+  trad_infix(A),
+  write(" et "),
+  trad_infix(B),
+  write(")").
+trad_infix(and(A,B)):-
+  write("("),
   trad_infix(A),
   write(" et "),
   trad_infix(B),
@@ -366,9 +423,21 @@ trad_infix((I,all(R,C))):-
   write("."),
   trad_infix(C),
   write(")").
+trad_infix(all(R,C)):-
+  write("qq("),
+  trad_infix(R),
+  write("."),
+  trad_infix(C),
+  write(")").
 trad_infix((I,some(R,C))):-
   write(I), 
   write(":ie("),
+  trad_infix(R),
+  write("."),
+  trad_infix(C),
+  write(")").
+trad_infix(some(R,C)):-
+  write("ie("),
   trad_infix(R),
   write("."),
   trad_infix(C),
@@ -378,9 +447,13 @@ trad_infix((I,or(A,B))):-
   write(":("),
   trad_infix(A),
   write(" ou "),
-  trad_infix(B),
+  trad_infix((B)),
   write(")").
-trad_infix(I):-
-  write("(atome:"),
-  write(I),
+trad_infix(or(A,B)):-
+  write("("),
+  trad_infix(A),
+  write(" ou "),
+  trad_infix((B)),
   write(")").
+trad_infix((I)):-
+  write(I).
